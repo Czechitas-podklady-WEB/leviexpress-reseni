efd407a012c69154c8ec795d7559249f39050418
Filip Jirsák <filip@jirsak.org>
2023-11-11 11:48:27 +0100

    LE1, detail cesty, cv.1: Komponenta BusStop.

--- src/components/JourneyDetail/index.jsx
+++ src/components/JourneyDetail/index.jsx
@@ -1,3 +1,4 @@
+import { BusStop } from "../BusStop";
 import "./style.css";
 
 export const JourneyDetail = () => {
@@ -5,38 +6,7 @@ export const JourneyDetail = () => {
     <div className="journey-detail container">
       <h2>Podrobnosti cesty</h2>
       <div className="stops">
-        <div className="bus-stop">
-          <div className="bus-stop__bullet"></div>
-          <div className="bus-stop__place">
-            <div className="bus-stop__city">Město 1</div>
-            <div className="bus-stop__station">Zastávka</div>
-          </div>
-          <div className="bus-stop__departure">10:45</div>
-        </div>
-        <div className="bus-stop">
-          <div className="bus-stop__bullet"></div>
-          <div className="bus-stop__place">
-            <div className="bus-stop__city">Město 2</div>
-            <div className="bus-stop__station">Zastávka</div>
-          </div>
-          <div className="bus-stop__departure">10:45</div>
-        </div>
-        <div className="bus-stop">
-          <div className="bus-stop__bullet"></div>
-          <div className="bus-stop__place">
-            <div className="bus-stop__city">Město 3</div>
-            <div className="bus-stop__station">Zastávka</div>
-          </div>
-          <div className="bus-stop__departure">10:45</div>
-        </div>
-        <div className="bus-stop">
-          <div className="bus-stop__bullet"></div>
-          <div className="bus-stop__place">
-            <div className="bus-stop__city">Město 4</div>
-            <div className="bus-stop__station">Zastávka</div>
-          </div>
-          <div className="bus-stop__departure">10:45</div>
-        </div>
+        <BusStop name="Praha" station="ÚAN Florenc" time="15:55" />
       </div>
     </div>
   );
