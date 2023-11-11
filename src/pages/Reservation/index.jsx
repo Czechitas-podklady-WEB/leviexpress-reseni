ff32dab016801e769bce4c7d0ff5b99d645edd0c
Filip Jirsák <filip@jirsak.org>
2023-11-11 14:11:35 +0100

    LE2, jízdenka, cv.3: Získání ID jízdenky z URL.

--- src/pages/Reservation/index.jsx
+++ src/pages/Reservation/index.jsx
@@ -1,7 +1,25 @@
+import { useParams } from "react-router-dom";
+
 export const Reservation = () => {
-    return (
-        <>
-        Zakoupená jízdenka TODO
-        </>
-    )
-}
+  const { id } = useParams();
+
+  return (
+    <div className="reservation container">
+      <h2>Vaše e-jízdenka č. {id}</h2>
+      <div className="reservation__body">
+        <div className="reservation__headings">
+          <p>Datum cesty:</p>
+          <p>Odjezd:</p>
+          <p>Příjezd:</p>
+          <p>Sedadlo:</p>
+        </div>
+        <div className="reservation__info">
+          <p>pá 28. květen 2021</p>
+          <p>Bratislava, 21:15</p>
+          <p>Budapest, 23:55</p>
+          <p>18</p>
+        </div>
+      </div>
+    </div>
+  );
+};
