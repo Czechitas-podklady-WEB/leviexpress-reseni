408a4c11ed0c45430df3a31b406272e51650aeb1
Filip Jirsák <filip@jirsak.org>
2023-11-11 13:46:18 +0100

    LE1, rezervace, cv.3: Zprovoznění objednávacího tlačítka.

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { JourneyPicker } from "../../components/JourneyPicker";
 import { JourneyDetail } from "../../components/JourneyDetail";
 import { SelectedSeat } from "../../components/SelectedSeat";
+import "./style.css"
 
 export const HomePage = () => {
   const [journey, setJourney] = useState(null);
@@ -10,13 +11,22 @@ export const HomePage = () => {
     setJourney(selectedJourney);
   };
 
+  const handleBuy = () => {
+    console.log("Objednávám.")
+  }
+
   return (
     <main>
       <JourneyPicker onJourneyChange={handleJourneyChange} />
       {journey && (
         <>
           <JourneyDetail journey={journey} />
-          <SelectedSeat number={journey.autoSeat} />          
+          <SelectedSeat number={journey.autoSeat} />
+          <div className="controls container">
+            <button className="btn btn--big" type="button" onClick={handleBuy}>
+              Rezervovat
+            </button>
+          </div>
         </>
       )}
     </main>
